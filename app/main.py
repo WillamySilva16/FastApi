4c4561a1ee3bf5d588a5633f43197756e9316d1b
Willamy Silva <willamycurso@gmail.com>
2025-12-03 11:17:39 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
-from db import buscar_visitas  # importa função que pega dados do BD
+from app.routes import router
 
 app = FastAPI()
+
+app.include_router(router)
 
 @app.get("/")
 def home():
     return {"status": "API online!"}
-
-@app.get("/visitas")
-def listar_visitas():
-    dados = buscar_visitas()
-    return dados
